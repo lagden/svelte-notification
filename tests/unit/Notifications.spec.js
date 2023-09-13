82bbf1810ca1205888c23e722d7c241efeaf1085
Thiago Lagden <lagden@gmail.com>
2023-09-13 16:48:57 -0300

    package stuff

--- tests/unit/Notifications.spec.js
+++ tests/unit/Notifications.spec.js
@@ -1,6 +1,5 @@
 import {setTimeout} from 'timers/promises'
 import {get} from 'svelte/store'
-import {tick} from 'svelte'
 import {test, expect, beforeEach, vi} from 'vitest'
 import {Notifications, acts} from '$lib/index.js'
 
@@ -23,8 +22,8 @@ test('Notifications', async () => {
 	const notification = {message: 'one', lifetime: 10}
 	acts.add(notification)
 
-	await setTimeout(2000)
-	const div = doc_query('div')
+	await setTimeout(100)
+	const div = doc_query('div._tadashi_svelte_notifications')
 	expect(div).toBeDefined()
 
 	const store = acts.getStore()
@@ -40,21 +39,21 @@ test('Notifications', async () => {
 
 	const clickEvent = new Event('click')
 	btn.dispatchEvent(clickEvent)
-	await tick()
-	await setTimeout(2000)
 
 	expect(spy, 'clicado').toHaveBeenCalledTimes(1)
+	await setTimeout(2500)
 	expect(() => doc_query('div._tadashi_svelte_notification__content')).toThrowError(/No element/)
 })
 
 test('Size', async () => {
 	const notification = {message: 'one', lifetime: 10}
 	acts.add(notification)
+	acts.add(notification)
+	acts.add(notification)
 
 	const target = doc_query('main#xxx')
 	new Notifications({target})
 
-	await setTimeout(2000)
-	const div = doc_query('div._tadashi_svelte_notification__content')
-	expect(div).toBeDefined()
+	const store = acts.getStore()
+	expect(get(store).size).toBe(3)
 })
